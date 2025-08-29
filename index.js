3179eae1347ef8c05c74c2842c873159a27ccf60
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-29 21:57:25 +0330

    Created posts_route.js with stub routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const postsRouter = require("./routes/posts_route.js");
 require("dotenv").config();
 const port = process.env.PORT;
+
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log(`Server: Listening on port ${port} ...`);
---
+++ routes/posts_route.js
@@ -0,0 +1,13 @@
+// Base route: /posts
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", () => {});
+router.post("/", () => {});
+
+router.get("/:id", () => {});
+router.put("/:id", () => {});
+router.delete("/:id", () => {});
+
+module.exports = router;
