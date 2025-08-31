404ba6a4e61078843ccd1228423da297abf64379
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-31 19:55:50 +0330

    Added error 404 and 405 for appropriate cases

--- models/response.js
+++ models/response.js
@@ -2,4 +2,29 @@ function makeResponseObj(success, message, data = {}) {
   return { success, message, data };
 }
 
-module.exports = { makeResponseObj };
+function send404Error(req, res) {
+  const resObj = makeResponseObj(false, "Not found");
+
+  return res.status(404).send(resObj);
+}
+
+function send405Error(allowedMethods) {
+  let allowHeaderValue = "";
+
+  for (const allowedMethod of allowedMethods) {
+    allowHeaderValue = `${allowHeaderValue}${allowedMethod}, `;
+  }
+  allowHeaderValue = allowHeaderValue.slice(0, -2);
+
+  return (req, res) => {
+    const resObj = makeResponseObj(
+      false,
+      `You can not ${req.method} ${req.baseUrl + req.path}`
+    );
+
+    res.set("Allow", allowHeaderValue);
+    return res.status(405).send(resObj);
+  };
+}
+
+module.exports = { makeResponseObj, send404Error, send405Error };
--- routes/posts_route.js
+++ routes/posts_route.js
@@ -8,13 +8,18 @@ const {
   getSinglePost,
   getPosts,
 } = require("../controllers/posts_controller");
+const { send404Error, send405Error } = require("../models/response");
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.all("/", send405Error(["GET", "POST"]));
 
 router.get("/:id", getSinglePost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
+router.all("/:id", send405Error(["GET", "PUT", "DELETE"]));
+
+router.all("/{*anything}", send404Error);
 
 module.exports = router;
